a610f69b1e83b476ac2bf153b2376cec501c8787
JuancaSterba <sjcexe@gmail.com>
2024-08-30 17:45:54 -0300

    se crea el proyecto

---
+++ src/main/java/com/itec1/FitPowerApplication.java
@@ -0,0 +1,13 @@
+package com.itec1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitPowerApplication.class, args);
+	}
+
+}
